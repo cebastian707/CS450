5794eb08b35ecce472a8ded4c9a01714fd6619ea
santiagohern1 <santiagohern1@llnl.gov>
2023-04-12 21:26:08 -0700

    Step 4 Done I think

---
+++ cmake-build-debug/CMakeFiles/APythonInterpreter.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/cebastiansantiago/CLionProjects/CS450/ExprNode.cpp" "CMakeFiles/APythonInterpreter.dir/ExprNode.cpp.o" "gcc" "CMakeFiles/APythonInterpreter.dir/ExprNode.cpp.o.d"
+  "/Users/cebastiansantiago/CLionProjects/CS450/Parser.cpp" "CMakeFiles/APythonInterpreter.dir/Parser.cpp.o" "gcc" "CMakeFiles/APythonInterpreter.dir/Parser.cpp.o.d"
+  "/Users/cebastiansantiago/CLionProjects/CS450/Statements.cpp" "CMakeFiles/APythonInterpreter.dir/Statements.cpp.o" "gcc" "CMakeFiles/APythonInterpreter.dir/Statements.cpp.o.d"
+  "/Users/cebastiansantiago/CLionProjects/CS450/SymTab.cpp" "CMakeFiles/APythonInterpreter.dir/SymTab.cpp.o" "gcc" "CMakeFiles/APythonInterpreter.dir/SymTab.cpp.o.d"
+  "/Users/cebastiansantiago/CLionProjects/CS450/Token.cpp" "CMakeFiles/APythonInterpreter.dir/Token.cpp.o" "gcc" "CMakeFiles/APythonInterpreter.dir/Token.cpp.o.d"
+  "/Users/cebastiansantiago/CLionProjects/CS450/Tokenizer.cpp" "CMakeFiles/APythonInterpreter.dir/Tokenizer.cpp.o" "gcc" "CMakeFiles/APythonInterpreter.dir/Tokenizer.cpp.o.d"
+  "/Users/cebastiansantiago/CLionProjects/CS450/main.cpp" "CMakeFiles/APythonInterpreter.dir/main.cpp.o" "gcc" "CMakeFiles/APythonInterpreter.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/APythonInterpreter.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "APythonInterpreter"
+  "APythonInterpreter.pdb"
+  "CMakeFiles/APythonInterpreter.dir/ExprNode.cpp.o"
+  "CMakeFiles/APythonInterpreter.dir/ExprNode.cpp.o.d"
+  "CMakeFiles/APythonInterpreter.dir/Parser.cpp.o"
+  "CMakeFiles/APythonInterpreter.dir/Parser.cpp.o.d"
+  "CMakeFiles/APythonInterpreter.dir/Statements.cpp.o"
+  "CMakeFiles/APythonInterpreter.dir/Statements.cpp.o.d"
+  "CMakeFiles/APythonInterpreter.dir/SymTab.cpp.o"
+  "CMakeFiles/APythonInterpreter.dir/SymTab.cpp.o.d"
+  "CMakeFiles/APythonInterpreter.dir/Token.cpp.o"
+  "CMakeFiles/APythonInterpreter.dir/Token.cpp.o.d"
+  "CMakeFiles/APythonInterpreter.dir/Tokenizer.cpp.o"
+  "CMakeFiles/APythonInterpreter.dir/Tokenizer.cpp.o.d"
+  "CMakeFiles/APythonInterpreter.dir/main.cpp.o"
+  "CMakeFiles/APythonInterpreter.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/APythonInterpreter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
